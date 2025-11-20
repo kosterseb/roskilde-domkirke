d9ffa401b2a1bcf8cf46f11b86b7265eb9255f14
kosterseb <sebkos.code@gmail.com>
2025-11-20 11:08:17 +0100

    Edit: landing page and header have been styled

--- src/components/HeaderNav.jsx
+++ src/components/HeaderNav.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
+import './HeaderNav.sass';
 
 export default function HeaderNav() {
     return (
         <header className="header-nav">
-        <nav>
-            <ul>
+        <nav className='navbar'>
+            <ul className='nav-links'>
             <li><a href="/">Home</a></li>
             <li><a href="/story">Story</a></li>
-            <li><a href="/equationsone">Equations One</a></li>
-            <li><a href="/equationstwo">Equations Two</a></li>
+            <li><a href="/equationsone">Example one</a></li>
+            <li><a href="/equationstwo">Example Two</a></li>
             <li><a href="/end">End</a></li>
             </ul>
         </nav>

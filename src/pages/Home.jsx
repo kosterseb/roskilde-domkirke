b7e0b3ab5bef66d660c83caaca92f2563fee88ff
kosterseb <sebkos.code@gmail.com>
2025-11-20 13:21:22 +0100

    Edit: made som edits to the home texts

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -14,9 +14,12 @@ export default function Home() {
                 <div>
                     <h2>A journey through time</h2>
                     <p>Embark on a journey through exciting stories about the amazing tales of our time,
-                        a royal institution with history and architecturial wonders.
+                        a royal institution with history and architecturial wonders. <br /><br />
+
                         In this showcase we will go through some history about <span className='highlight'>Roskilde
                             Domkirke</span> and what needed to be rebuild and how.</p>
+
+                    <p className="disclaimer">*This is for educational purposes only*</p>
                     <Link to="/story" className="start-button">Start Your Adventure →</Link>
                 </div>
             </div>

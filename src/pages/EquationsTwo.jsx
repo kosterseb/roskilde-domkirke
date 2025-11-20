9b7c0b597a3df24ac3dca431bf597fede3026de2
Christian Reed <reed@Christians-MacBook-Pro.local>
2025-11-20 13:39:20 +0100

    feat: added math questions to the dots

--- src/pages/EquationsTwo.jsx
+++ src/pages/EquationsTwo.jsx
@@ -1,10 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import HeaderNav from '../components/HeaderNav.jsx';
 import { Link } from 'react-router';
 import InfoDot from '../components/InfoDot.jsx'; // Import the new component
 import './EquationsTwo.sass';
 
 export default function EquationsTwo() {
+	const [ep1Message, setEp1Message] = useState('');
+
+	function handleSubmitEq1(event) {
+		event.preventDefault();
+		const answer = event.target.answerInput.value;
+		if (answer === '5') {
+			setEp1Message(
+				'Correct! - Fun fact: The cathedral consists of approximately 2.5 million red bricks.'
+			);
+		} else {
+			setEp1Message('Incorrect, please try again.');
+		}
+	}
 	return (
 		<>
 			<HeaderNav />
@@ -12,20 +25,39 @@ export default function EquationsTwo() {
 				<h1>Equations Two</h1>
 				<div className="equations-two-page__calculation-container">
 					<InfoDot top="33.5%" left="55%">
-						<p>Solve for x: 2x + 3 = 11</p>
+						<p>Find the hypotenuse of the triangle?</p>
+						<p>Given: A right triangle with legs (a & b) of 3m and 4m</p>
+						<p>
+							Find the hypotenuse using the Pythagorean theorem. a² + b² = c²
+						</p>
+						<form onSubmit={handleSubmitEq1}>
+							<input
+								type="text"
+								placeholder="Your answer here"
+								className="input"
+								name="answerInput"
+							/>
+							<input type="submit" value="Submit" className="submitBtn" />
+						</form>
+						{ep1Message && <p className="feedback-message">{ep1Message}</p>}
 					</InfoDot>
 
 					<InfoDot top="55%" left="65%">
-						<p>Solve for y: 5y - 4 = 16</p>
+						<p>
+							The amazing roof structure is built from 350m³ of timber. Mostly
+							douglas fir but also oak.
+						</p>
 					</InfoDot>
 
 					<InfoDot top="40%" left="80%">
-						<p>Solve for z: 3z + 2 = 14</p>
+						<p>
+							The Cathedral is one of the most important monuments in Denmark.
+						</p>
+						<p>The Cathedral was built in the 12th and 13th centuries</p>
+						<p>
+							-and has been the burial church of the royal family for centuries.
+						</p>
 					</InfoDot>
-					<p>
-						Welcome to the second set of equations! Solve these to continue your
-						adventure.
-					</p>
 				</div>
 
 				<p>
